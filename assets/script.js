51ce342bb115b1ea7335931b7e673137b35ec237
dannabelleli <belleli.danna@gmail.com>
2023-03-25 15:40:00 +0300

    added more js code

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,32 @@
 let topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-console.log(topics);
+let randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (let i = 0; i > topics.length; i++) {
-    console.log(topics[i]);
+let listTopics = function () {
+    for (let i = 0; i < topics.length; i++) {
+        console.log(topics[i]);
+    }
 }
 
-
-/*
-for (let i = 0; i > topics.length; i++) {
-    if (i === i[0]) {
+let selectTopic = function () {
+    if (randomTopic === 'HTML') {
         console.log("Let's study HTML!");
-    } else if (i === i[1]) {
+    } else if (randomTopic === 'CSS') {
         console.log("Let's study CSS!");
-    } else if (i === i[2]) {
+    } else if (randomTopic === 'Git') {
         console.log("Let's study Git!");
-    } else if (i === i[3]) {
+    } else if (randomTopic === 'JavaScript') {
         console.log("Let's study JavaScript!");
     } else {
         console.log('Please try again!');
     }
 }
-*/
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
+
+
 
 
 
