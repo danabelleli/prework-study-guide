a56d8c5e634873d1cd6eccd58506aaa254943243
dannabelleli <belleli.danna@gmail.com>
2023-03-25 15:21:42 +0300

    added js

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+let topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+console.log(topics);
+
+for (let i = 0; i > topics.length; i++) {
+    console.log(topics[i]);
+}
+
+
+/*
+for (let i = 0; i > topics.length; i++) {
+    if (i === i[0]) {
+        console.log("Let's study HTML!");
+    } else if (i === i[1]) {
+        console.log("Let's study CSS!");
+    } else if (i === i[2]) {
+        console.log("Let's study Git!");
+    } else if (i === i[3]) {
+        console.log("Let's study JavaScript!");
+    } else {
+        console.log('Please try again!');
+    }
+}
+*/
+
+
+
